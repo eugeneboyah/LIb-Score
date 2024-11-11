523b08efa4cde0d6fe88f774652a6e8f2dde1fd6
Eugene Boyah <boyaheugene1999@gmail.com>
2024-11-11 21:46:19 +0000

    set up node envro..

--- server.js
+++ server.js
@@ -14,6 +14,30 @@ app.get('/', (req, res ) => {
     res.render('index'); 
 });
 
+app.get('/live', (req, res ) => {
+    res.render('live-game'); 
+});
+
+app.get('/fixture', (req, res ) => {
+    res.render('matches-fixture'); 
+});
+
+app.get('/result', (req, res ) => {
+    res.render('matches-result'); 
+});
+
+app.get('/sign-in', (req, res ) => {
+    res.render('sign-in'); 
+});
+
+app.get('/sign-up', (req, res ) => {
+    res.render('sign-up'); 
+});
+
+app.get('/admin', (req, res ) => {
+    res.render('dashboard'); 
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`);
